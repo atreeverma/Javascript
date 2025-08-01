f3abe9517e4ca7fe3ad62d54072a135551847b95
Atree Verma <88149132+atreeverma@users.noreply.github.com>
2025-08-01 11:14:24 +0000

    commit

--- bs/1.js
+++ bs/1.js
@@ -1,5 +1,14 @@
-const id=123;
-let acc="sgvdfv"
-var account="sdfv"
-city="sdgvs"
-console.table([id,acc,account,city]);
+//const id="123";
+//let acc="sgvdfv"
+//var account="sdfv"
+//city="sdgvs"
+//console.table([id,acc,account,city]);
+//console.log(typeof id);
+//let check=Number(false)
+//console.log(check);
+//console.log("2"+1+1);
+//const i=Symbol('123')
+//const chec=Symbol('123')
+//console.log(i===chec);
+
+
