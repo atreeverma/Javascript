a6558720834e3c45d0ba41c2597727b33419c671
Atree Verma <88149132+atreeverma@users.noreply.github.com>
2025-08-01 15:58:00 +0000

    comm

--- bs/array.js
+++ bs/array.js
@@ -1,20 +1,20 @@
-const arr1=[1,2,'dfgfd']
-const arr2=new Array(1,2,3,4)
-arr1.push(2)
-arr2.push(3)
-arr1.shift()
-arr2.shift()
-console.log(arr1);
-console.log(arr2);
-console.log(arr1.indexOf(2));
-const newarr=arr1.join()
-console.log(newarr);
-const myarr=arr2.slice(1,3)
-//const myarr1=arr2.splice(1,3)
-console.log(myarr);
-//console.log(myarr1);
-console.log(arr2);
-console.log("s",arr1);
+// const arr1=[1,2,'dfgfd']
+// const arr2=new Array(1,2,3,4)
+// arr1.push(2)
+// arr2.push(3)
+// arr1.shift()
+// arr2.shift()
+// console.log(arr1);
+// console.log(arr2);
+// console.log(arr1.indexOf(2));
+// const newarr=arr1.join()
+// console.log(newarr);
+// const myarr=arr2.slice(1,3)
+// //const myarr1=arr2.splice(1,3)
+// console.log(myarr);
+// //console.log(myarr1);
+// console.log(arr2);
+// console.log("s",arr1);
 
 
 
