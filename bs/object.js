29e28516aa4d092f388ba0f56137641449664e84
Atree Verma <88149132+atreeverma@users.noreply.github.com>
2025-08-01 17:45:27 +0000

    comm

---
+++ bs/object.js
@@ -0,0 +1,25 @@
+// const sym=Symbol("key")
+// const JsUser={
+//     name:"hitesh",
+//     age:12,
+//     location:"asd",
+//     islogg:false,
+//     [sym]:"fd"
+// }
+// // console.log(JsUser["name"]);
+// // console.log(JsUser.age);
+// // console.log(JsUser[sym]);
+// JsUser.greeting=function(){
+//     console.log("hello");
+// }
+// JsUser.greeting()
+// JsUser.greetingtow=function(){
+//     console.log(`hello${this.age}`);
+// }
+// JsUser.greetingtow()
+//object singleton
+
+
+
+
+
