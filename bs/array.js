d270b43cadb56d53d2269b9bbce43d9cb2440afe
Atree Verma <88149132+atreeverma@users.noreply.github.com>
2025-08-01 15:53:13 +0000

    comm

---
+++ bs/array.js
@@ -0,0 +1,14 @@
+const arr1=[1,2,'dfgfd']
+const arr2=new Array(1,2,3,4)
+arr1.push(2)
+arr2.push(3)
+arr1.shift()
+arr2.shift()
+console.log(arr1);
+console.log(arr2);
+console.log(arr1.indexOf(2));
+const newarr=arr1.join()
+console.log(newarr);
+
+
+
