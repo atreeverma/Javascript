d4ddab94b2887dbc2c233c6fab38bec40e9ff592
Atree Verma <88149132+atreeverma@users.noreply.github.com>
2025-08-01 14:42:12 +0000

    comm

---
+++ bs/number.js
@@ -0,0 +1,40 @@
+//Number
+// const score=400
+// const balance=new Number(10000000)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(4));
+// console.log(balance.toLocaleString(en-In));
+//
+//Maths
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.floor(23.67));
+// console.log(Math.min(23,32,1,41,3,23,2));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*6+1));
+// console.log(Math.floor(Math.random()*max-min+1));
+
+//Date and Time
+// let mydate=new Date()
+// // console.log(mydate)
+// // console.log(mydate.toDateString());
+// // console.log(mydate.toString());
+//  let mydaty=new Date(2025,1,23,2,4)
+// console.log(mydaty.toString());
+// let dates=new Date("1-3-2045")
+// console.log(dates.toDateString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ bs/string.js
@@ -0,0 +1,25 @@
+//  const name="sdgfsdg"
+//  const repo=1
+// console.log(repo+name+"value");
+// console.log(`jvkjxv ${name} nmdfk ${repo}`);
+// const gamename=new String('svsvs')
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.length);
+// console.log(gamename.charAt(2));
+// const newstring=gamename.substring(0,4)
+// console.log(newstring);
+// const st=" fdf  dfd     dg       "
+// console.log(st.trim());
+// const che="&********"
+// console.log(che.replace('*','&'))
+// console.log(che.includes('hitesh'));
+
+
+
+
+
+
+
+
