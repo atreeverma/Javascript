d084a3af584c5dacfd5ebe0245f29b197c9b388c
Atree Verma <88149132+atreeverma@users.noreply.github.com>
2025-08-04 07:42:44 +0000

    comm

---
+++ bs/function.js
@@ -0,0 +1,37 @@
+// function add(n1,n2){
+//     console.log(n1+n2);
+// }
+// add(3,4)
+// add(3,'2')
+// const no=add(3,2)
+// console.log(no);
+// function login(username){
+//     return `${username}sdgsb`
+// }
+// console.log(login("afdf"));
+// function login(username){
+//     if(!username){
+//         console.log("enter username");
+//         return
+//     }
+//     return `${username}`
+// }
+// console.log(login());
+// function login(username="sma"){
+//     if(!username){
+//         console.log("enter username");
+//         return
+//     }
+//     return `${username}`
+// }
+// console.log(login());
+// function cal(...num1){
+//     return num1
+// }
+// console.log(cal(1,2,3,4));
+// function call(val1,val2,...num1){
+//     return num1
+// }
+// console.log(call());
+
+
