47b01b665dc5fd80fe4e8c04e345fcf6f0bc9e7a
Atree Verma <88149132+atreeverma@users.noreply.github.com>
2025-08-04 12:29:21 +0000

    comm

--- bs/function.js
+++ bs/function.js
@@ -34,4 +34,78 @@
 // }
 // console.log(call());
 
+// const use={
+//     username:"sgsfg",
+//     price:223,
+// }
+// function call(object){
+//     console.log(`${object.username}  ${object.price}`);
+// }
+// call(use)
 
+// function call(object){
+//     console.log(`${object.username}  ${object.price}`);
+// }
+// call({
+//     username:"sgsfg",
+//     price:223,
+// })
+
+// const array=[1,2,3,4]
+// function call(arr){
+//     return arr[1]
+// }
+// console.log(call(array));
+// console.log(call([1,3,3,4,5,5]));
+
+// console.log(add(5));
+// function add(num){
+//     return num+1
+// }
+// console.log(val1(4));
+// const va11=function(num){ ///error
+//     return num+1
+// }
+
+///this 
+// const user={
+//     username:"dgdg",
+//     price:23,
+
+//     welcome:function(){
+//         console.log(`${this.username}`);
+//         //console.log(this);
+//     }
+// }
+// user.welcome()
+// //console.log(this);
+// user.username="sam"
+// user.welcome()
+
+
+// function call(){
+//     console.log(this);
+// }
+// call()
+
+// function call(){
+//     let username="sdgvs"
+//     console.log(this.username);
+// }
+// call()
+
+//Arrow function
+// const chai=()=>{
+//     let username="dfd"
+//     console.log(this.username);
+//     console.log(this);
+// }
+// chai()
+
+// const chai=(nums1,nums2)=>{
+//     return nums1+nums2
+// }
+// console.log(chai(2,3));
+
+// const chai=(num1,num2)=> (num1+num2)
+// console.log(chai(2,3));
