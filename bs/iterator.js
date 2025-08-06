d583998e79206249db5923384134c62b8830da6f
Atree Verma <88149132+atreeverma@users.noreply.github.com>
2025-08-06 04:00:54 +0000

    comm

---
+++ bs/iterator.js
@@ -0,0 +1,22 @@
+// const arr=[1,2,3,4]
+// for (const i of arr) {
+//     console.log(i);
+// }
+
+// Maps
+
+const map=new Map()
+map.set('IN',"India")
+// console.log(map);
+
+// for (const key of map) {
+//     console.log(key);
+    
+// }
+// for (const [key,value] of map) {
+//     console.log(key, ':-' ,value);
+// }
+
+const obj={
+    
+}
